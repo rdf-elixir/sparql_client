04af8c275cb54671a1cd672b6902448f34bb0872
Marcel Otto <marcelotto@gmx.de>
2020-09-17 11:51:15 +0200

    Move exvcr config to dev environment config

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,3 @@
 use Mix.Config
 
-config :exvcr,
-  vcr_cassette_library_dir: "integration_test/fixtures/vcr_cassettes",
-  custom_cassette_library_dir: "integration_test/fixtures/custom_cassettes"
-
 import_config "#{Mix.env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,9 @@
 use Mix.Config
 
+config :exvcr,
+  vcr_cassette_library_dir: "integration_test/fixtures/vcr_cassettes",
+  custom_cassette_library_dir: "integration_test/fixtures/custom_cassettes"
+
 config :tesla, :adapter, Tesla.Adapter.Hackney
 # config :tesla, :adapter, Tesla.Adapter.Gun
 # config :tesla, :adapter, Tesla.Adapter.Mint
