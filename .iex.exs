59771c2afb5f6396b05d34cacf79a65ac431cd9a
Marcel Otto <marcelotto@gmx.de>
2020-09-21 13:31:37 +0200

    Add .iex.exs file

---
+++ .iex.exs
@@ -0,0 +1,39 @@
+import RDF.Sigils
+import RDF.Guards
+
+alias RDF.NS
+alias RDF.NS.{RDFS, OWL, SKOS}
+
+alias RDF.{
+  Term,
+  IRI,
+  BlankNode,
+  Literal,
+  XSD,
+
+  Triple,
+  Quad,
+  Statement,
+
+  Description,
+  Graph,
+  Dataset,
+
+  PrefixMap,
+}
+
+alias RDF.BlankNode, as: BNode
+
+alias RDF.{NTriples, NQuads, Turtle}
+
+alias Decimal, as: D
+
+defmodule Test do
+  use RDF.Vocabulary.Namespace
+  defvocab EX, base_iri: "http://example.com/", terms: [], strict: false
+end
+
+alias Test.EX
+
+alias SPARQL.Client
+import SPARQL.Client
