2b14ca749fc49f044364ee800698a417d6badf2a
Marcel Otto <marcelotto@gmx.de>
2023-03-02 21:38:40 +0100

    Update dev dependencies

--- mix.exs
+++ mix.exs
@@ -93,11 +93,11 @@ defmodule SPARQL.Client.Mixfile do
       {:gun, "~> 1.3", only: [:dev, :test]},
       {:mint, "~> 1.2", only: [:dev, :test]},
       {:castore, "~> 0.1", only: [:dev, :test]},
-      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:exvcr, "~> 0.13", only: [:dev, :test]},
-      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.14", only: :test}
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.15", only: :test}
     ]
   end
 
